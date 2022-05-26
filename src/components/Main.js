9acfbeab6227a5390f52be522fa1d5ada973afb5
petragliamateo <petragliamateo@gmail.com>
2022-05-26 12:58:23 -0300

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Main(){
+    return(
+        <div className="Main">
+            <h2>Hi! My Name Is</h2>
+            <h1>
+                Mateo
+            </h1>
+            <h2>Front End Developer</h2>
+
+            <div className="socialIcons">
+                <a href="#!">
+                    <i className="fab fa-twitter fa-2x"></i>
+                </a>
+                <a href="#!">
+                    <i className="fab fa-facebook fa-2x"></i>
+                </a>
+                <a href="#!">
+                    <i className="fab fa-linkedin fa-2x"></i>
+                </a>
+                <a href="#!">
+                    <i className="fab fa-github fa-2x"></i>
+                </a>
+            </div>
+        </div>
+    )
+    
+}
+
+export default Main;
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Navbar(){
+    return(
+        <div className="Navbar">
+            <button className="btn btn-primary">
+                asdqwe
+            </button>
+        
+            <ul>
+                <li>
+                    <a href="#!">Home</a>
+                </li>
+                <li>
+                    <a href="#!">About</a>
+                </li>
+                <li>
+                    <a href="#!">Me</a>
+                </li>
+                <li>
+                    <a href="#!">Projects</a>
+                </li>
+                <li>
+                    <a href="#!">Contact Me</a>
+                </li>
+            </ul>
+        </div>
+    )
+    
+}
+
+export default Navbar;
